7150fa286faaceccefea2ffca3ee83920ea73d8c
Felipe C. Araujo <felipe.s-04@hotmail.com>
2023-07-15 13:46:43 -0300

    change auth screen

--- lib/screens/auth/auth_screen.dart
+++ lib/screens/auth/auth_screen.dart
@@ -13,62 +13,82 @@ class AuthScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-              labelText: 'Email',
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Theme.of(context).colorScheme.primary,
-                  width: 2,
-                ),
-                borderRadius: BorderRadius.circular(16),
-              ),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(16),
-              ),
-            ),
-            onChanged: (newEmail) {
-              email = newEmail;
-            },
-          ),
-          const SizedBox(height: 16),
-          TextField(
-            onChanged: (newPassword) {
-              password = newPassword;
-            },
-            obscureText: true,
-            decoration: InputDecoration(
-              labelText: 'Senha',
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Theme.of(context).colorScheme.primary,
-                  width: 2,
-                ),
-                borderRadius: BorderRadius.circular(16),
-              ),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(16),
-              ),
-            ),
-          ),
-          const SizedBox(height: 32),
-          ElevatedButton(
-            onPressed: () => _authenticateUser(
-              context: context,
-              email: email,
-              password: password,
-            ),
-            child: const Text('Login'),
-          ),
-        ],
+      appBar: AppBar(
+        title: const Text(
+          'Bem vindo!',
+          style: TextStyle(color: Colors.deepPurple),
+        ),
+        backgroundColor: Theme.of(context).primaryColor.withOpacity(0.15),
       ),
-    ));
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: 'Email',
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Theme.of(context).colorScheme.primary,
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+              onChanged: (newEmail) {
+                email = newEmail;
+              },
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              onChanged: (newPassword) {
+                password = newPassword;
+              },
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Senha',
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Theme.of(context).colorScheme.primary,
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+            ),
+            const SizedBox(height: 32),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                fixedSize: Size(MediaQuery.of(context).size.width * 0.9, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+              onPressed: () => _authenticateUser(
+                context: context,
+                email: email,
+                password: password,
+              ),
+              child: const Text('Login'),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: TextButton(
+          onPressed: () => Navigator.pushNamed(context, MenuScreen.routeName),
+          child: const Text('Nao tenho conta, criar uma'),
+        ),
+      ),
+    );
   }
 
   Future<void> _authenticateUser({
@@ -83,19 +103,21 @@ class AuthScreen extends StatelessWidget {
           email: email,
           password: password,
         )
-        .then((value) => Navigator.pushReplacementNamed(context, MenuScreen.routeName))
+        .then((value) =>
+            Navigator.pushReplacementNamed(context, MenuScreen.routeName))
         .onError(
           (error, stackTrace) => authInstance
               .createUserWithEmailAndPassword(email: email, password: password)
-              .then(
-                  (value) => Navigator.pushReplacementNamed(context, MenuScreen.routeName))
+              .then((value) =>
+                  Navigator.pushReplacementNamed(context, MenuScreen.routeName))
               .onError(
                 (error, stackTrace) =>
                     ScaffoldMessenger.of(context).showSnackBar(
                   SnackBar(
                     backgroundColor: Colors.red,
                     content: Text(
-                      (error as FirebaseException).message ?? 'Erro desconhecido',
+                      (error as FirebaseException).message ??
+                          'Erro desconhecido',
                     ),
                   ),
                 ),
